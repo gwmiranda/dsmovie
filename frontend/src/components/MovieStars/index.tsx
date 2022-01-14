fc354d6cad1afe9253e5e61838c651a8fb76d3dc
Guilherme Miranda <gwdemiranda@outlook.com>
2022-01-14 11:19:19 -0300

    Retirada import em MovieStars/index

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,7 @@ import {ReactComponent as StarFull} from 'asserts/img/star-full.svg';
 import {ReactComponent as StarHalf} from "asserts/img/star-half.svg";
 import {ReactComponent as StarEmpty} from "asserts/img/star-empty.svg";
 import './styles.css'
-import { type } from 'os';
+
 
 type Props = {
     score: number
