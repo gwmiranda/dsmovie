e6d0a81c33a6cb98ddbcc7df9171ec4547972636
Guilherme Miranda <gwdemiranda@outlook.com>
2022-01-14 10:46:45 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 function Listing() {
     
     const [pageNumber, setPageNumber] = useState(0);
+    
     const [page, setPage] = useState<MoviePage>({
         content: [],
         last: true,
@@ -29,20 +30,15 @@ function Listing() {
             });
 
     }, [pageNumber]); 
-    
-    
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
 
-    
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
+        
     return (
         <>
-            <Pagination/>
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className={"row"}>
                     {page.content.map(movie => (
